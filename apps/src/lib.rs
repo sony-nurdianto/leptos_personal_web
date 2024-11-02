a8c5bbe566b123f5326c5019a08d564b2aa85cdc
sony-nurdianto <sonynurdianto445@gmail.com>
2024-11-02 15:49:46 +0700

    adding stylance

--- apps/src/lib.rs
+++ apps/src/lib.rs
@@ -1,21 +1,22 @@
 mod pages;
 
+use crate::pages::{HomePage, NotFoundPage};
 use leptos::{component, view, IntoView};
 use leptos_meta::{provide_meta_context, Stylesheet, Title};
-use leptos_router::{Router, Routes, Route};
-use crate::pages::{HomePage, NotFoundPage};
+use leptos_router::{Route, Router, Routes};
 
 #[component]
 pub fn App() -> impl IntoView {
     provide_meta_context();
     view! {
-        <Stylesheet id="leptos" href="/pkg/personal_web.css"/>
-        <Title text="Sony Nurdianto"/>
+        <Stylesheet id="leptos" href="/pkg/personal_web.css" />
+        <Title text="Sony Nurdianto" />
         <Router>
             <Routes>
-                <Route path="" view=HomePage/>
-                <Route path="/*any" view=NotFoundPage/>
+                <Route path="" view=HomePage />
+                <Route path="/*any" view=NotFoundPage />
             </Routes>
         </Router>
     }
 }
+
